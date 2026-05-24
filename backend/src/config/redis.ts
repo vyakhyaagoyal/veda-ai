9f99c52e280ddf0163aef3badd7e7e3b5c47c0cb
Vyakhya Goyal <vyakhyatwentytwo@gmail.com>
2026-05-25 00:35:05 +0530

    changed env

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -3,6 +3,7 @@ import IORedis from "ioredis";
 export const connection = new IORedis({
   host: process.env.REDIS_HOST,
   port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASSWORD,
 
   maxRetriesPerRequest: null,
 });
--- backend/src/queues/redis.ts
+++ backend/src/queues/redis.ts
@@ -3,6 +3,7 @@ import IORedis from "ioredis";
 export const redisConnection = new IORedis({
   host: process.env.REDIS_HOST,
   port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASSWORD,
 
   maxRetriesPerRequest: null,
 });

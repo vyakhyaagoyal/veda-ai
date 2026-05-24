f048b698e6d9ff955559573998d196e7656bf98e
Vyakhya Goyal <vyakhyatwentytwo@gmail.com>
2026-05-25 00:16:07 +0530

    updated next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "veda-ai-production-39b3.up.railway.app",
+      },
+    ],
+  },
 };
 
 export default nextConfig;

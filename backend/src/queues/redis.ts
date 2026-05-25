6121492fb3ef54a96c51f2c4a20193b72e1f2096
Vyakhya Goyal <vyakhyatwentytwo@gmail.com>
2026-05-25 16:30:18 +0530

    fix redis

--- backend/src/queues/redis.ts
+++ backend/src/queues/redis.ts
@@ -1,9 +1,10 @@
 import IORedis from "ioredis";
 
-export const redisConnection = new IORedis({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-  password: process.env.REDIS_PASSWORD,
-
-  maxRetriesPerRequest: null,
-});
+export const redisConnection =
+  new IORedis(
+    process.env.REDIS_URL as string,
+    {
+      maxRetriesPerRequest:
+        null,
+    }
+  );
